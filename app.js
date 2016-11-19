36b0d926004a2ba6ce2f2f0a0c4e5d57aea7c57f
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2016-11-18 22:56:11 -0600

    vue is dope!

---
+++ app.js
@@ -0,0 +1,126 @@
+const store = new Vuex.Store({
+
+    state: {
+        currentPage: '/login',
+        user: null
+    },
+
+    mutations: {
+
+        setUser: (state, payload) => {
+            state.user = payload;
+        },
+        setPage: (state, newPage) => {
+            state.currentPage = newPage;
+        }
+
+    }
+
+});
+
+Vue.component('sign-in-page', {
+
+    template: '#signInPageTemplate'
+
+});
+
+Vue.component('sign-in-form', {
+
+    template: '#signInFormTemplate',
+
+    data: function() {
+        return {
+            username: '',
+            password: '',
+            isLoading: false,
+            errorMessage: '',
+            errorMessages: {
+                blankFields: 'Username and password are required.'
+            }
+        };
+    },
+
+    computed: {
+        signInButtonClasses: function() {
+            return [
+                'button',
+                'is-primary',
+                'is-pulled-right',
+                'is-medium',
+                {
+                    'is-loading': this.isLoading,
+                    'is-disabled': this.hasEmptyFields
+                }
+            ];
+        },
+        inputClasses: function() {
+            return [
+                'input',
+                'is-medium',
+                {
+                    'is-danger': this.highlightInput
+                }
+            ];
+        },
+        hasEmptyFields: function(){
+            return this.username == '' || this.password == '';
+        },
+        highlightInput: function(){
+            return this.errorMessage == this.errorMessages.blankFields;
+        }
+    },
+
+    methods: {
+        loginUser: function() {
+            var self = this;
+
+            if(self.isLoading)
+                return;
+
+            if(self.hasEmptyFields)
+            {
+                self.errorMessage = self.errorMessages.blankFields;
+            }
+            else
+            {
+                self.errorMessage = '';
+                self.isLoading = true;
+                setTimeout(function(){
+                    store.commit('setUser', {
+                        username: self.username, 
+                        password: self.password
+                    });
+                    store.commit('setPage', '/app/collections');
+                    self.isLoading = true;
+                }, 1000);
+            }
+
+        }
+    }
+
+});
+
+Vue.component('collections-page', {
+    template: '#collectionsPageTemplate'
+});
+
+Vue.component('navbar', {
+    template: '#navbarTemplate',
+
+    methods: {
+        signOut: function(){
+            store.commit('setUser', null);
+            store.commit('setPage', '/login');
+        }
+    }
+});
+
+new Vue({
+    el: '#app',
+
+    computed: {
+        currentPage: function() {
+            return store.state.currentPage;
+        }
+    }
+});
