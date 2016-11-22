5248f272b8bc3384a8bdc2bcf8a0118f813466c6
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2016-11-22 00:44:55 -0600

    this is so easy

--- app.js
+++ app.js
@@ -5,6 +5,27 @@ const store = new Vuex.Store({
         user: null
     },
 
+    getters: {
+        view: function(state){
+
+            switch(state.currentPage)
+            {
+                case '/app/collections':
+                    return 'collections-page';
+            }
+
+            return 'sign-in-page';
+        },
+        navHeader: function(state){
+            switch(state.currentPage)
+            {
+                case '/app/collections':
+                    return 'Collections';
+            }
+            return '';
+        }
+    },
+
     mutations: {
 
         setUser: (state, payload) => {
@@ -71,7 +92,7 @@ Vue.component('sign-in-form', {
     },
 
     methods: {
-        loginUser: function() {
+        signInUser: function() {
             var self = this;
 
             if(self.isLoading)
@@ -107,30 +128,95 @@ Vue.component('collections-page', {
 Vue.component('navbar', {
     template: '#navbarTemplate',
 
-    data: function(){
-        return {
-            navHeader: 'Collections'
-        };
+    computed: {
+
+        navHeader: function(){
+            return store.getters.navHeader;
+        }
     },
 
     methods: {
         signOut: function(){
             store.commit('setUser', null);
-            store.commit('setPage', '/login');
+            store.commit('setPage', '/sign-in');
         }
     }
 });
 
 Vue.component('side-nav', {
-    template: '#sideNavTemplate'
+    template: '#sideNavTemplate',
+
+    data: function(){
+        return {
+            collections: [
+                { label: 'Animals' },
+                { label: 'Plants' },
+                { label: 'Dinosaurs' },
+                { label: 'Aliens' }
+            ],
+            filterQuery: '',
+            allCollectionsLabel: 'All Collections'
+        };
+    },
+
+    computed: {
+        alphabetizedCollections: function(){
+            return this.collections.sort( function(x,y) {
+                return x.label < y.label;
+            });
+        },
+        filteredCollections: function(){
+            var self = this;
+            return self.collections.filter( function(collection) {
+                return collection.label.toLowerCase().includes(self.filterQuery.toLowerCase());
+            });
+        },
+        displayFilteredResults: function(){
+            return this.filterQuery.length > 2;
+        },
+        noFilterResults: function(){
+            return this.filteredCollections.length == 0;
+        },
+        filterSectionLabel: function(){
+
+            var prefix = 'Results for "';
+            var middle = '';
+            var suffix = '":';
+
+            if (this.filterQuery.length > 10)
+                middle = this.filterQuery.substring(0,9) + '...';
+            else
+                middle = this.filterQuery;
+
+            return prefix + middle + suffix;
+        },
+        navHeader: function(){
+            return store.getters.navHeader;
+        }
+    }
+
+});
+
+Vue.component('side-nav-menu-section', {
+
+    template: '#sideNavMenuSectionTemplate',
+
+    props: ['headerLabel', 'collections'],
+
+    methods: {
+        collectionSelected: function(collectionLabel) {
+            console.log(collectionLabel);
+        }
+    }
+
 });
 
 new Vue({
     el: '#app',
 
     computed: {
-        currentPage: function() {
-            return store.state.currentPage;
+        view: function() {
+            return store.getters.view;
         }
     }
 });
